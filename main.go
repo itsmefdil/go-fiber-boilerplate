65ba766d7e48d506520a703717e0b95ae43278bc
Fadilah Riczky <friczky@gmail.com>
2023-09-23 15:58:23 +0700

    add healthCheck and middleware

--- main.go
+++ main.go
@@ -3,20 +3,46 @@ package main
 import (
 	"fiber/config/database"
 	"fiber/router"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
+	// Fiber instance
 	app := fiber.New()
 
+	// Connect to database
 	database.ConnectDB()
 
+	// Middleware
+	app.Use(recover.New())
+	app.Use(cors.New())
+
+	// Routes
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
+	app.Get("/health", HealthCheck)
+
 	router.SetupRoutes(app)
 
-	app.Listen(":3000")
+	if err := app.Listen(":3000"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func HealthCheck(c *fiber.Ctx) error {
+	res := map[string]interface{}{
+		"data": "Server is up and running",
+	}
+
+	if err := c.JSON(res); err != nil {
+		return err
+	}
+
+	return nil
 }
